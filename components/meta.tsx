a810f75217623385ea6335e56191c748f8521bfe
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-11-10 10:10:56 +0530

    fix: :chart_with_upwards_trend: google analytics configured

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,13 +1,22 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Meta() {
   return (
-    <Head>
-      <script
-        defer
-        data-domain="akilesh.io"
-        src=""//plausible.io/js/plausible.js"
-      />
-    </Head>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-6ZVPLRHTGQ"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6ZVPLRHTGQ');
+        `}
+      </Script>
+    </>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Meta from '@/components/meta'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Meta />
+      <Component {...pageProps} />
+    </>
+  )
 }
