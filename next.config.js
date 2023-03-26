bba1611c76eca11d734da7b33b830adbe13c0b66
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-03-26 14:13:14 +0530

    chore: :wrench: next conf
    removed un used config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/socket',
-        destination: 'https://app-ruscello.vercel.app/api/socket',
-      },
-    ]
-  }
 }
 
 module.exports = nextConfig
