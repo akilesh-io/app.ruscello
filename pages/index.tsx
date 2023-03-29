24306f071f4ef1639c95531cf5db59f4a65ca5c5
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-03-30 00:10:01 +0530

    fix:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useState , useContext } from "react";
+import React, { useState } from "react";
 
 import styles from "@/styles/Home.module.css";
 import Layout from "@/components/layout";
