ef473b902c9fb4fdb484dadb3bdf4e05b92c00bf
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-03-29 23:36:38 +0530

    fix: :art: user name UI added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useState , useContext } from "react";
 
-import styles from '@/styles/Home.module.css'
-import Layout from '@/components/layout'
+import { UserNameContext } from "@/context/userName";
+import styles from "@/styles/Home.module.css";
+import Layout from "@/components/layout";
 
 export default function Home() {
+  const router = useRouter();
+  const [userName, setUserName] = useState("");
+  const { setUserName1 } = useContext(UserNameContext);
 
-  const router = useRouter()
-
-  const joinRoom = () => {
-    router.push(`/room/${Math.random().toString(36).slice(2)}`)
-  }
+  const joinRoom = (e) => {
+    e.preventDefault();
+    //localStorage.setItem('userName', userName);
+    setUserName1(userName);
+    console.log("🚀 ~ file: index.tsx:19 ~ joinRoom ~ setUserName1:", setUserName1)
+    router.push(`/room/${Math.random().toString(36).slice(2)}`);
+  };
 
   return (
     <div className={styles.container}>
@@ -21,14 +28,29 @@ export default function Home() {
           <link rel="icon" href="/favicon.png" />
         </Head>
 
-        <div className="flex justify-center align-middle items-center">
-            <button onClick={joinRoom} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+        <form className="flex flex-col justify-center" onSubmit={joinRoom}>
+          <div className="flex justify-center align-middle items-center">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="User Name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+             // required
+            />
+          </div>
+
+          <div className="flex justify-center align-middle items-center">
+            <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Create a room
               </span>
             </button>
-        </div>
+          </div>
+        </form>
       </Layout>
     </div>
-  )
+  );
 }
