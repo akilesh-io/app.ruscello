314f6bc772c2bc1319cafc53301d12fc4afbd6be
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-04-10 19:37:59 +0530

    fix: :lipstick: removed css and added tailwind

--- components/FaceTime.tsx
+++ components/FaceTime.tsx
@@ -5,7 +5,6 @@ import { socket } from "@/context/socketUrl";
 
 import Draggable from "react-draggable";
 
-import styles from "@/styles/VideoCall.module.css";
 
 import Mic from "@/public/svg/mic.svg";
 import MicOff from "@/public/svg/mic off.svg";
@@ -32,26 +31,27 @@ const FaceTime = () => {
   }
 
   return (
-    <div className={styles.container}>
-      <Draggable bounds="parent">
-        <div className="cursor-grab flex flex-col justify-end items-end fixed bottom-2 right-4 space-y-10">
-          <div className="flex flex-row ">
-            <video
-              id="localVideo"
-              className="w-40 bg-blue-100 rounded border-blue-400 mr-2"
-              autoPlay
-              muted
-            />
+    <div>
+      <div className="h-screen p-2 z-50">
+        <Draggable bounds="parent">
+          <div className="cursor-grab flex flex-col justify-end items-end fixed bottom-2 right-4 space-y-10">
+            <div className="flex flex-row ">
+              <video
+                id="localVideo"
+                className="w-40 bg-blue-100 rounded border-blue-400 mr-2"
+                autoPlay
+                muted
+              />
 
-            <video
-              id="remoteVideo"
-              className="w-40 bg-green-100 rounded border-green-400"
-              autoPlay
-            />
+              <video
+                id="remoteVideo"
+                className="w-40 bg-green-100 rounded border-green-400"
+                autoPlay
+              />
+            </div>
           </div>
-        </div>
-      </Draggable>
-
+        </Draggable>
+      </div>
       <div className="flex flex-row justify-center items-center fixed bottom-0 w-full space-x-10">
         <button onClick={toggleMic} type="button">
           {micActive ? (
