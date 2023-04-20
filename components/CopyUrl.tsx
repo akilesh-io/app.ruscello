0efb21d22f46fbad58b2e875ae5527656a2f09fc
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-04-20 21:10:07 +0530

    refactor: :art: copy to clip board
    Copy url is been now seperated from main and created a component

---
+++ components/CopyUrl.tsx
@@ -0,0 +1,42 @@
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import DoneIcon from "@mui/icons-material/Done";
+
+export default function CopyUrl() {
+  const router = useRouter();
+
+  const [copy, setCopy] = useState(false);
+
+  const fullPath =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "https://app-ruscello.vercel.app";
+
+  if (copy) {
+    setTimeout(() => {
+      setCopy(false);
+    }, 2000);
+  }
+
+  return (
+    <div className="m-2">
+      <CopyToClipboard
+        text={fullPath + router.asPath}
+        onCopy={() => setCopy(true)}
+      >
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 "
+        >
+          {copy ? (
+            <DoneIcon className="w-5 h-5" fontSize="small" />
+          ) : (
+            <ContentCopyIcon className="w-5 h-5" fontSize="small" />
+          )}
+        </button>
+      </CopyToClipboard>
+    </div>
+  );
+}
--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -3,9 +3,8 @@ import dynamic from "next/dynamic";
 import { useEffect, useRef } from "react";
 import { socket } from "@/context/socketUrl";
 import { useRouter } from "next/router";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import screenfull from "screenfull";
+import CopyUrl from "@/components/CopyUrl";
 
 const FaceTime = dynamic(() => import("@/components/FaceTime"), { ssr: false });
 const FileUpload = dynamic(() => import("@/components/FileUpload"));
@@ -14,11 +13,6 @@ export default function Room() {
   const router = useRouter();
   const videoAndFace = useRef<any>(null);
   const videoAlone = useRef<any>(null);
-
-  const fullPath =
-    typeof window !== "undefined" && window.location.origin
-      ? window.location.origin
-      : "https://app-ruscello.vercel.app";
 
   const { id: roomName } = router.query;
 
@@ -40,17 +34,13 @@ export default function Room() {
 
   return (
     <Layout>
-      <div className="m-2 top-0 left-0">
-        <CopyToClipboard text={fullPath + router.asPath}>
-          <ContentCopyIcon fontSize="large" />
-        </CopyToClipboard>
-      </div>
-        <div className="w-full h-full" ref={videoAndFace}>
-          <div className="top-0 left-0 w-full h-full" ref={videoAlone}>
-            <FileUpload />
-          </div>
-          <FaceTime />
+      <CopyUrl />
+      <div className="w-full h-full" ref={videoAndFace}>
+        <div className="top-0 left-0 w-full h-full" ref={videoAlone}>
+          <FileUpload />
         </div>
+        <FaceTime />
+      </div>
     </Layout>
   );
 }
