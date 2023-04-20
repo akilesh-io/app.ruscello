67e402a5cb90266b6dac007be816f4dc8d2f24e4
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-04-20 21:13:05 +0530

    style: :lipstick: Custom css is been apped to application

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#146173",
+        secondary: "#F22233",
+        tertiary: "#BFAE99",
+        primaryLight: "#16808C",
+        secondaryLight: "#D97777",
+      }
+    },
   },
   plugins: [],
 }
