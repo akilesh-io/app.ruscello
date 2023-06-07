22b24d83474f3ba5878b5fe16a4134f5d6afd7cb
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-06-07 19:44:07 +0530

    perf: :mag: meta tag is meen updated

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,8 +1,23 @@
-import Script from 'next/script'
+import Script from "next/script";
 
 export default function Meta() {
   return (
     <>
+      <title>Filmingo</title>
+      <meta
+        property="og:description"
+        content="Filmingo - Experience the joy of watching movies together online with Filmingo. Stream and enjoy synchronized movie viewing with friends from anywhere."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://filmingo.me" />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/davkfrmah/image/upload/v1686146984/Filmingo/temp_film_bvuced.jpg"
+      />
+      <meta property="og:image:alt" content="Filmingo" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
       <Script
         async
         src="https://scripts.simpleanalyticscdn.com/latest.js"
@@ -17,5 +32,5 @@ export default function Meta() {
         />
       </noscript>
     </>
-  )
+  );
 }
