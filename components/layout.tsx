efc0426568125a2fc834b930ce957d81e5b92160
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-06-11 08:18:34 +0530

    fix: :bookmark: meta tag is been switcjed

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 type Props = {
   children: React.ReactNode;
 };
@@ -7,14 +5,6 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <>
-      <Head>
-        <title>Filmingo</title>
-        <meta
-          name="description"
-          content="Discover a captivating movie-watching experience with Filmingo. Watch and enjoy films together with friends in real-time, creating lasting memories. Explore a vast library of films, sync up with loved ones, and immerse yourself in the world of cinematic entertainment like never before."
-        />
-        <link rel="icon" href="https://res.cloudinary.com/davkfrmah/image/upload/c_scale,h_48/v1684239381/Filmingo/flamingo_logo.png"></link>
-      </Head>
       <div>
         <div className="min-h-screen">
           <main>{children}</main>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Filmingo</title>
+        <meta
+          name="description"
+          content="Discover a captivating movie-watching experience with Filmingo. Watch and enjoy films together with friends in real-time, creating lasting memories. Explore a vast library of films, sync up with loved ones, and immerse yourself in the world of cinematic entertainment like never before."
+        />
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/davkfrmah/image/upload/c_scale,h_48/v1684239381/Filmingo/flamingo_logo.png"
+        ></link>
+
         <Meta />
       </Head>
       <Component {...pageProps} />
